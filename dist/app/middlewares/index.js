808db03f8c240f93d8acc9795aedae054ddedffa
Qhris-tian <christian.kojobarnes@gmail.com>
2023-07-02 16:11:28 +0000

    update gitignore

--- dist/app/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ValidationMiddleware = void 0;
-const validation_1 = __importDefault(require("./validation"));
-exports.ValidationMiddleware = validation_1.default;
